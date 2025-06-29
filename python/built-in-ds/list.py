cd270f7cfc3ed7ce4cc73e700d3b2cd11b3fb7f4
Sathesh Raj <satheshrajsde@gmail.com>
2025-06-29 05:52:33 +0000

    List practiced

---
+++ python/built-in-ds/list.py
@@ -0,0 +1,22 @@
+# Simple list to check #
+
+simple_list=[1,2,3,"Check",'check_s']
+print(simple_list)
+
+print("Simple list is created")
+
+# Adding element to List 
+
+simple_list.append("appended_element")
+print(simple_list)
+
+print("Element is appended")
+
+# creating empty list 
+
+empty_list = []
+print(empty_list)
+
+#Added value to empty list
+empty_list.append("Added_value")
+print(empty_list)
